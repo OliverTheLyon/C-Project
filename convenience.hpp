2a6fc4776952fa106be79074592b8f618ef45a1b
teddy2021 <darkking@live.ca>
2020-07-22 13:25:04 -0600

    Minor changes to names and includes

---
+++ convenience.hpp
@@ -0,0 +1,11 @@
+#ifndef CON
+#define CON
+#include <stdio.h>
+#include <string.h>
+#include <string>
+#include <fstream>
+#endif
+
+using namespace std;
+
+bool ReadFile(const char* file, string& out);
